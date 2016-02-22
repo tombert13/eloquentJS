04074d107f8c55de6f08762dccef876cfaaeb953
Tom <tombertrand1407@gmail.com>
2016-02-22 12:57:00 +0100

    add missing functions

--- chapter6.js
+++ chapter6.js
@@ -16,18 +16,18 @@ function Vector(x, y) {
 };
 
 Vector.prototype.plus = function (vector) {
-	return new Vector (this.x + vector.x, this.y + vector.y);
+	return new Vector(this.x + vector.x, this.y + vector.y);
 };
 
 Vector.prototype.minus = function (vector) {
-	return new Vector (this.x - vector.x, this.y - vector.y);
+	return new Vector(this.x - vector.x, this.y - vector.y);
 };
 
 var vector1 = new Vector(2, 5);
 var vector2 = new Vector(2, 8);
 
 var res = vector1.plus(vector2);
-console.log(res);
+//console.log(res);
 
 /*Another cell
 Implement a cell type named StretchCell(inner, width, height) that conforms
@@ -37,19 +37,50 @@ cell has at least the given width and height, even if the inner cell would
 naturally be smaller.*/
 
 function StretchCell(inner, width, height) {
-  this.inner = inner;
-  this.width = width;
-  this.height = height;
+	this.inner = inner;
+	this.width = width;
+	this.height = height;
 }
 
-StretchCell.prototype.minWidth = function() {
-  return Math.max(this.width, this.inner.minWidth());
+function repeat(string, times) {
+	var result = "";
+	for (var i = 0; i < times; i++)
+		result += string;
+	return result;
+}
+
+function TextCell(text) {
+	this.text = text.split("\ n ");
+}
+
+TextCell.prototype.minWidth = function () {
+	return this.text.reduce(function (width, line) {
+		return Math.max(width, line.length);
+	}, 0);
 };
-StretchCell.prototype.minHeight = function() {
-  return Math.max(this.height, this.inner.minHeight());
+TextCell.prototype.minHeight = function () {
+	return this.text.length;
 };
-StretchCell.prototype.draw = function(width, height) {
-  return this.inner.draw(width, height);
+TextCell.prototype.draw = function (width, height) {
+	var result = [];
+	for (var i = 0; i < height; i++) {
+		var line = this.text[i] || "";
+		result.push(line + repeat(" ", width - line.length));
+	}
+	return result;
+};
+
+
+StretchCell.prototype.minWidth = function () {
+	return Math.max(this.width, this.inner.minWidth());
+};
+
+StretchCell.prototype.minHeight = function () {
+	return Math.max(this.height, this.inner.minHeight());
+};
+
+StretchCell.prototype.draw = function (width, height) {
+	return this.inner.draw(width, height);
 };
 
 var sc = new StretchCell(new TextCell("abc"), 1, 2);
