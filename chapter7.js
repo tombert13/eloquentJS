2ae4449ecfb5bac18816db12672a9913c3e06217
Tom <tombertrand1407@gmail.com>
2016-02-22 15:22:36 +0100

    Grid object, with some basic methods

--- chapter7.js
+++ chapter7.js
@@ -1,21 +1,54 @@
 var plan = ["############################",
-            "#      #    #      o      ##",
-            "#                          #",
-            "#          #####           #",
-            "##         #   #    ##     #",
-            "###           ##     #     #",
-            "#           ###      #     #",
-            "#   ####                   #",
-            "#   ##       o             #",
-            "# o  #         o       ### #",
-            "#    #                     #",
-            "############################"];
-			
+    "#      #    #      o      ##",
+    "#                          #",
+    "#          #####           #",
+    "##         #   #    ##     #",
+    "###           ##     #     #",
+    "#           ###      #     #",
+    "#   ####                   #",
+    "#   ##       o             #",
+    "# o  #         o       ### #",
+    "#    #                     #",
+    "############################"];
+
 function Vector(x, y) {
-	this.x = x;
-	this.y = y;
+    this.x = x;
+    this.y = y;
 }
 
 Vector.prototype.plus = function (other) {
-	return new Vector(this.x + other.x, this.y + other.y);
+    return new Vector(this.x + other.x, this.y + other.y);
 };
+
+var grid = [" top left ", " top middle ", " top right ",
+    " bottom left ", " bottom middle ", " bottom right "];
+
+console.log(grid[2 + (1 * 3)]);
+// → bottom right
+
+function Grid(width, height) {
+    this.space = new Array(width * height);
+    this.width = width;
+    this.height = height;
+}
+
+Grid.prototype.isInside = function (vector) {
+    return vector.x >= 0 && vector.x < this.width &&
+        vector.y >= 0 && vector.y < this.height;
+};
+
+Grid.prototype.get = function (vector) {
+    return this.space[vector.x + this.width * vector.y];
+};
+
+Grid.prototype.set = function (vector, value) {
+    this.space[vector.x + this.width * vector.y] = value;
+};
+
+//And here is a trivial test:
+var grid = new Grid(5, 5);
+console.log(grid.get(new Vector(1, 1)));
+// → undefined
+grid.set(new Vector(1, 1), " X ");
+console.log(grid.get(new Vector(1, 1)));
+// → X
