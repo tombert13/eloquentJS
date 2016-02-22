3ef2e7f4187910fa62746267e035add151dfe765
Tom <tombertrand1407@gmail.com>
2016-02-22 15:08:57 +0100

    data for chapter 7

--- chapter7.js
+++ chapter7.js
@@ -1,7 +1,21 @@
+var plan = ["############################",
+            "#      #    #      o      ##",
+            "#                          #",
+            "#          #####           #",
+            "##         #   #    ##     #",
+            "###           ##     #     #",
+            "#           ###      #     #",
+            "#   ####                   #",
+            "#   ##       o             #",
+            "# o  #         o       ### #",
+            "#    #                     #",
+            "############################"];
+			
 function Vector(x, y) {
 	this.x = x;
 	this.y = y;
 }
+
 Vector.prototype.plus = function (other) {
 	return new Vector(this.x + other.x, this.y + other.y);
 };
