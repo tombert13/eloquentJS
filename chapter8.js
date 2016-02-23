aa2c2160854980364bf15cdce2695cc32ad5065b
Tom <tombertrand1407@gmail.com>
2016-02-23 10:54:35 +0100

    chapter 8
    how to throw/catch an error

--- chapter8.js
+++ chapter8.js
@@ -16,11 +16,29 @@ function numberToString(n, base) {
 	}
 	do {
 		result = String(n % base) + result;
-		n = Math.floor(n/base);
+		n = Math.floor(n / base);
 	} while (n > 0);
 	return sign + result;
 }
-console.log(numberToString(1325, 10));
+//console.log(numberToString(1325, 10));
+
+function promptDirection(question) {
+	var result = prompt(question, "");
+	if (result.toLowerCase() == " left ") return " L ";
+	if (result.toLowerCase() == " right ") return " R ";
+	throw new Error(" Invalid direction : " + result);
+}
+function look() {
+	if (promptDirection(" Which way ?") == " L ")
+		return " a house ";
+	else
+		return " two angry bears ";
+}
+try {
+	console.log(" You see ", look());
+} catch (error) {
+	console.log(" Something went wrong : " + error);
+}
 
 
 exports.canYouSpotTheProblem = canYouSpotTheProblem;
